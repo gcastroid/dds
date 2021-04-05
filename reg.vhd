cf2f559a5cf0416cd5b7f1574c14429fad5a085b
gcastroid <57358057+gcastroid@users.noreply.github.com>
2021-04-05 11:38:26 -0300

    Update reg.vhd

--- reg.vhd
+++ reg.vhd
@@ -4,10 +4,10 @@ use ieee.std_logic_1164.all;
 entity reg is 
    generic(Nbits: integer);
    port(
-	i_rst: in std_logic;
-	i_clk: in std_logic;
-	i_en: in std_logic;
-	i_data: in std_logic_vector(Nbits-1 downto 0);
+   i_rst: in std_logic;
+   i_clk: in std_logic;
+   i_en: in std_logic;
+   i_data: in std_logic_vector(Nbits-1 downto 0);
    o_data: out std_logic_vector(Nbits-1 downto 0));
 end entity;
 
@@ -18,16 +18,16 @@ architecture behave of reg is
 begin
 
    process(i_rst, i_clk)
-	begin
-	   if (i_rst = '0') then
-		   r_data <= (others => '0');
-		elsif (rising_edge(i_clk)) then
-		   if (i_en = '1') then
-			   r_data <= i_data;
-			end if;
-		end if;
-	end process;
+   begin
+      if (i_rst = '0') then 
+         r_data <= (others => '0');
+      elsif (rising_edge(i_clk)) then
+         if (i_en = '1') then
+       	    r_data <= i_data;
+	 end if;
+      end if;
+   end process;
 	
-	o_data <= r_data;
+   o_data <= r_data;
 
 end architecture;
